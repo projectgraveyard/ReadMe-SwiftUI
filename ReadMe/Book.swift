55b5b89ad34ce0c38cbc6f4f6ec9ac32ac5ec027
Dominic Swaine <dominic.swaine@gmail.com>
2021-06-26 18:12:32 +0100

    Create Book data model

---
+++ ReadMe/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  ReadMe
+//
+//  Created by Dominic Swaine on 26/06/2021.
+//
+
+struct Book {
+    let title: String
+    let author: String
+    
+    init(title: String = "Title", author: String = "Author") {
+        self.title = title
+        self.author = author
+    }
+}
