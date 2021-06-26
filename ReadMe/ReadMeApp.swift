175c232a9f4750f16925dd1a4ffbd015e987c723
Dominic Swaine <dominic.swaine@gmail.com>
2021-06-26 17:30:06 +0100

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Dominic Swaine on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
