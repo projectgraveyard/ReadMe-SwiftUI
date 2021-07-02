47afbce03227bd37cca61b5bede983c57f7e4e1e
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-02 21:37:58 +0100

    Add microReview, readMe fields to the Book model

--- ReadMe/Book.swift
+++ ReadMe/Book.swift
@@ -8,9 +8,18 @@
 struct Book: Hashable {
     let title: String
     let author: String
+    var microReview: String
+    var readMe: Bool
     
-    init(title: String = "Title", author: String = "Author") {
+    init(
+        title: String = "Title",
+        author: String = "Author",
+        microReview: String = "",
+        readMe: Bool = true
+    ) {
         self.title = title
         self.author = author
+        self.microReview = microReview
+        self.readMe = readMe
     }
 }
