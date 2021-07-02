8e4cf56b75590131f90f5cd4de7aa142a85d6c33
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-02 22:51:19 +0100

    Refactor DetailView by creating ReviewAndImageStack

--- ReadMe/Views/DetailView.swift
+++ ReadMe/Views/DetailView.swift
@@ -11,8 +11,6 @@ import class PhotosUI.PHPickerViewController
 struct DetailView: View {
     @ObservedObject var book: Book
     @Binding var image: UIImage?
-    @State var showingImagePicker = false
-    @State var showingDeleteAlert = false
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -24,51 +22,10 @@ struct DetailView: View {
                     authorFont: .title2
                 )
             }
-            VStack {
-                Divider()
-                    .padding(.vertical)
-                TextField("Review...", text: $book.microReview)
-                Divider()
-                    .padding(.vertical)
-                Book.Image(
-                    uiImage: image,
-                    title: book.title,
-                    cornerRadius: 16
-                )
-                .scaledToFit()
-                
-                let updateButton = Button(
-                    "Update Image",
-                    action: {showingImagePicker = true}
-                )
-                
-                if image != nil {
-                    HStack {
-                        Spacer()
-                        Button(
-                            "Delete Image",
-                            action: {showingDeleteAlert = true}
-                        )
-                        Spacer()
-                        updateButton
-                        Spacer()
-                    }
-                } else {updateButton}
+            ReviewAndImageStack(book: book, image: $image)
 
-            }
-            Spacer()
         }
         .padding()
-        .sheet(isPresented: $showingImagePicker) {
-            PHPickerViewController.View(image: $image)
-        }
-        .alert(isPresented: $showingDeleteAlert) {
-            .init(
-                title: .init("Delete image for \(book.title)?"),
-                primaryButton: .destructive(.init("Delete")){image = nil},
-                secondaryButton: .cancel()
-            )
-        }
     }
 }
 
---
+++ ReadMe/Views/ReviewAndImageStack.swift
@@ -0,0 +1,60 @@
+//
+//  ReviewAndImageStack.swift
+//  ReadMe
+//
+//  Created by Dominic Swaine on 02/07/2021.
+//
+
+import SwiftUI
+
+struct ReviewAndImageStack: View {
+    @ObservedObject var book: Book
+    @Binding var image: UIImage?
+    @State var showingImagePicker = false
+    @State var showingDeleteAlert = false
+    
+    var body: some View {
+        VStack {
+            Divider()
+                .padding(.vertical)
+            TextField("Review...", text: $book.microReview)
+            Divider()
+                .padding(.vertical)
+            Book.Image(
+                uiImage: image,
+                title: book.title,
+                cornerRadius: 16
+            )
+            .scaledToFit()
+            
+            let updateButton = Button(
+                "Update Image",
+                action: {showingImagePicker = true}
+            )
+            
+            if image != nil {
+                HStack {
+                    Spacer()
+                    Button(
+                        "Delete Image",
+                        action: {showingDeleteAlert = true}
+                    )
+                    Spacer()
+                    updateButton
+                    Spacer()
+                }
+            } else {updateButton}
+            Spacer()
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            PHPickerViewController.View(image: $image)
+        }
+        .alert(isPresented: $showingDeleteAlert) {
+            .init(
+                title: .init("Delete image for \(book.title)?"),
+                primaryButton: .destructive(.init("Delete")){image = nil},
+                secondaryButton: .cancel()
+            )
+        }
+    }
+}
